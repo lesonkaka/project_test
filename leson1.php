49450c14b412f6a635613e9dd0c37d86acd5a995
vb_sonle <vb.sonle@gmail.com>
2020-10-30 13:58:14 +0700

    Merge branch 'leson3' into leson1
    # Conflicts:
    #	leson1.php

--- leson1.php
+++ leson1.php
@@ -1,11 +1,4 @@
 <?php
-echo 22222;
-echo __FILE__;
-echo "toitoi";
-echo 2222222;
-echo "kikiki";
-echo 3333;
-echo "aaaaa";
-echo 44444;
-echo 6666666666666666666;
-echo 55555;
+
+echo "leson3";
+echo 333333333;
