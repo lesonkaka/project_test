26a72493ec171d7f6327a9c3ffa92ee710985929
vb_sonle <vb.sonle@gmail.com>
2020-10-30 13:59:01 +0700

    no message

--- leson1.php
+++ leson1.php
@@ -1,18 +1,3 @@
 <?php
-echo 22222;
-echo 123;
-echo __FILE__;
-echo "toitoi";
-
-echo 2222222;
-echo "kikiki";
-echo 3333;
-echo "aaaaa";
-echo 44444;
-echo 55555;
-echo "hahaha";
-echo 6666;
-echo 7777;
-echo 898989;
-echo 8888;
-echo 999999999999999999;
+echo "leson1";
+echo 111111;
